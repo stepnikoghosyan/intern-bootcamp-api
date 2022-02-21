550f726ffe00d8057d01545f9558dadc0b605c67
Stepan <snikoghosyan@servicetitan.com>
2022-02-21 18:52:04 +0400

    change verify account url

--- src/shared/helpers/send-account-verification-email.helper.ts
+++ src/shared/helpers/send-account-verification-email.helper.ts
@@ -33,6 +33,7 @@ export async function sendAccountVerificationEmail(data: {
 
   const accountVerificationUrl = [
     WEB_DOMAIN,
+    'auth',
     'verify-account',
     activationToken,
   ].join('/');
