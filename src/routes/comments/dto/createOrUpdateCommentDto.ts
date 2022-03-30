88c1deb71b1c483d19965bc1dc98d630eae65280
Stepan <snikoghosyan@servicetitan.com>
2022-03-30 15:35:49 +0400

    maxlength validation for comment message

--- src/routes/comments/dto/createOrUpdateCommentDto.ts
+++ src/routes/comments/dto/createOrUpdateCommentDto.ts
@@ -1,9 +1,10 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateOrUpdateCommentDto {
   @IsString()
   @IsNotEmpty()
+  @MaxLength(255)
   @ApiProperty()
   message: string;
 }
